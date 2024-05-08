238f017c0de0c7d40e7ab920e9c26b8ba5192e25
Souei <57272851+Soueii@users.noreply.github.com>
2024-05-08 23:33:51 +0300

    Add files via upload
    Trying to get user data and push it into an array. Still have to polish that up.

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.use(express.static("public"));
 
+app.post("/submit", (req, res, next) => {
+  let titleValue = req.body.fTitle;
+  let descValue = req.body.fDescription;
+
+  let storedData = [];
+  let userData = req.body;
+
+  storedData.push(userData);
+
+  res.send("Data stored successfully!");
+  next();
+
+  res.render("index.ejs", {
+    title: titleValue,
+    description: descValue,
+  });
+});
+
 app.get("/", (req, res) => {
+  // res.json(storedData);
   res.render("index.ejs");
 });
 
